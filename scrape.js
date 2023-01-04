9195369f344173f793ffe186483ce747b1ff9cc8
jianshn <107050251+jianshn@users.noreply.github.com>
2023-01-04 16:17:24 +0800

    Update

--- scrape.js
+++ scrape.js
@@ -27,6 +27,7 @@ async function realProcess(xhr) {
 
         const data = JSON.parse(xhr.responseText)['actions'][0]['returnValue']['factMap'];
         var keys = Object.keys(data);
+        console.log(keys);
         console.log(JSON.parse(xhr.responseText));
         
         for (var j in keys) {
