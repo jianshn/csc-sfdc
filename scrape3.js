76d1c02a32c7a736a1ed3c9f8bcca8a423a548fa
jianshn <107050251+jianshn@users.noreply.github.com>
2023-01-10 15:54:35 +0800

    Update scrape3.js

--- scrape3.js
+++ scrape3.js
@@ -84,7 +84,7 @@ async function realProcess(xhr) {
         
         console.log(tmp_list);
         console.log('Get ddb items');
-        let ddbItems = opp_more_than_one_hour();
+        let ddbItems = await opp_more_than_one_hour();
         console.log(ddbItems);
         console.log('end of result')
     }
@@ -96,11 +96,11 @@ class NotificationUrl {
     }
 }
 
-function opp_more_than_one_hour() {
+async function opp_more_than_one_hour() {
     const params = {
         TableName: 'Test'
     };
-    const result = docClient.scan(params, function(err, data) {
+    const result = await docClient.scan(params, function(err, data) {
         if (err) console.log(err);
         else console.log(data);
     });
@@ -108,6 +108,7 @@ function opp_more_than_one_hour() {
     var ddb_list = [];
 
     for (item in result.Item) {
+        console.log(item);
         ddb_list.push(item['sfdc_id'])
     }
     return ddb_list;
