f56bd93944780640eb69748f8af86981013edb69
jianshn <107050251+jianshn@users.noreply.github.com>
2023-01-11 15:21:50 +0800

    Update scrape3.js

--- scrape3.js
+++ scrape3.js
@@ -108,14 +108,8 @@ async function realProcess(xhr) {
         
         // compare opp in tmp list to ddb table, add time if present, 0 = sfdc_id, 1 = time_in_sfdc
         while (i < ddbItems.ddb_list.length) {
-            for (opp in tmp_list) {
-                console.log(opp);
-                console.log(ddbItems.ddb_list[i][0]);
-                if (opp == ddbItems.ddb_list[i][0]) {
-                    ddbItems.ddb_list[i][1] = ddbItems.ddb_list[i][1] + 30;
-                    await updateToDB(ddbItems.ddb_list[i][0],ddbItems.ddb_list[i][1])
-                }
-            }
+            console.log(ddbItems.ddb_list[i][0]);
+
             i++;
         }
 
